6cdae6b439db2c2b9305220836796528e108a81e
pramodnivid <pramod.km@nividapps.com>
2018-02-23 11:43:56 +0530

    new

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,3 @@
-
-// const path = require('path')
-
-// const path1 = path.join(__dirname,"public");
-
-// module.exports = {
-//     entry: "./src/app.js",
-//     output: {
-//         path: path1,
-//         filename:"bundle.js"},
-//     module:{
-//         rules:[{
-//             loader:'babel-loader',
-//             test: /\.js$/,
-//             exclude: /node_modules/
-//         },{
-//             test: /\.s?css$/,
-//             use:['style-loader','css-loader','sass-loader']
-//         }]
-//     },
-//     devtool:'cheap-module-eval-source-map',
-//     devServer:{
-//         contentBase:path1,
-//         historyApiFallback:true
-//     }
-// }
-
-
-
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = (env) => {
